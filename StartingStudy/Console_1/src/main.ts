b9ceb0cd50da46b4f7af0304a09ffcf2133cd73d
Igor.Sokolov <I.Sokolov@ya.ru>
2020-05-10 09:40:32 +0300

    model list

--- StartingStudy/Console_1/src/main.ts
+++ StartingStudy/Console_1/src/main.ts
@@ -6,44 +6,77 @@ import { AccessToken, ConnectClient, HubIModel, IModelHubClient, Project, IModel
 
 import { Config } from "./Config";
 
-const actx = new ClientRequestContext("");
 
-export class MyApp
-{
-    public static get clientConfig (): bkclient.OidcAgentClientConfiguration {
+
+  ///////////////////////////////////////////////////////////////////////
+  /*
+  private static get clientConfig (): bkclient.OidcAgentClientConfiguration {
         return {
-          clientId: "service-3gCb5lDykM5MdfbUkRVVHC6IQ",
-          clientSecret: "JvSTYIBwMJBZ8NPO/cLfjVrnizH/+Iz7H7OgTI6abPqp4rZn1GujJByUvFrL56pan7yMNEQRkNyZ4thAsh+F0Q==",
+          clientId: process.IMODELJS_USER,
+          clientSecret: "Nnygx40qY35BaWJZFstuwmeoDC8Up4NLM2hOAdlvrl9KzGT8MS+MOnYO80YCkX5XZqzuQFhVx9rJ94a84eaDjQ==",
           scope: "urlps-third-party context-registry-service:read-only imodelhub",
         };
     }
+  */
+ const clientConfig : bkclient.OidcAgentClientConfiguration = {
+    clientId: process.env.iModeljsAgentId!,
+    clientSecret: process.env.iModeljsAgentSecret!,
+    scope: "urlps-third-party context-registry-service:read-only imodelhub",
+  };
 
   ///////////////////////////////////////////////////////////////////////
-  public static async main(){
-    Config.startup ();
+  const connectClient : ConnectClient = new ConnectClient();
+  let authCtx : AuthorizedClientRequestContext;
+  let hubClient : IModelHubClient;
 
-    // Log in
-    Logger.logTrace(Config.loggingCategory, `Attempting to login to OIDC for ${MyApp.clientConfig.clientId}`);
-    const client = new OidcAgentClient(MyApp.clientConfig);
-    const accessToken: AccessToken = await client.getToken(actx);
-    Logger.logTrace(Config.loggingCategory, `Successful login`);
-
-    const authCtx = new AuthorizedClientRequestContext(accessToken!);
-    const connectClient = new ConnectClient();
-
-    //get project
-    try 
-    {
-      const project = await connectClient.getProject(authCtx, {$select: "*", });      
-    } 
-    catch (error) 
-    {
-      Logger.logError (Config.loggingCategory, error);
-    }
-
-    Config.shutdown();
+  ///////////////////////////////////////////////////////////////////////
+  /*async*/ function ProcessModel (model : HubIModel){
+    Logger.logTrace (Config.loggingCategory, "    " + model.name);
+    //this._iModelDb = await this._iModelDbHandler.openLatestIModelDb(authCtx, this._projectId!, this._iModelId!);
   }
-}
+  
+  ///////////////////////////////////////////////////////////////////////
+  async function ProcessProject (project : Project) /*: Promise<void>*/{
+    Logger.logTrace (Config.loggingCategory, "  Project " + project.name);
+
+    Logger.logTrace (Config.loggingCategory, "  Models:");
+    hubClient = new IModelHubClient(new AzureFileHandler());    
+
+    const imodels:HubIModel[] = await hubClient.iModels.get (authCtx!, project.wsgId/*, new IModelQuery().byName("iModelName")*/);
+   
+    imodels.forEach(ProcessModel);
+  }
+
+  ///////////////////////////////////////////////////////////////////////
+  async function main(){
+    try {
+      Config.startup ();
+
+      // Log in
+      Logger.logTrace(Config.loggingCategory, `Attempting to login to OIDC for ${clientConfig.clientId}`);
+      const client = new OidcAgentClient(clientConfig);
+      const actx = new ClientRequestContext("");
+      const accessToken: AccessToken = await client.getToken(actx);
+      Logger.logTrace(Config.loggingCategory, `Successful login`);
+
+      authCtx = new AuthorizedClientRequestContext(accessToken!);
+
+      //get projects
+      Logger.logTrace (Config.loggingCategory, "Get acessible projects");
+      const projects = await connectClient.getProjects(authCtx, {
+                                                                        $select: "*", 
+                                                                        /*$filter: "Name+eq+'" + projectName + "'",*/
+                                                                        });            
+      projects.forEach(ProcessProject);
+
+      Config.shutdown();
+    }
+    catch (error)  {
+      Logger.logError (Config.loggingCategory, error);
+    }    
+  Logger.logTrace (Config.loggingCategory, "Finish getting projects");
+  }
+
 
 //////////////////////////////////////////////////////////
 Logger.initializeToConsole();
@@ -53,6 +86,6 @@ Logger.logTrace(Config.loggingCategory, "Logger initialized...");
 if (require.main === module) {
   // Logger.logTrace(Config.loggingCategory, "Step in main");
   // tslint:disable-next-line:no-floating-promises
-  MyApp.main();
+  main();
   // Logger.logTrace(Config.loggingCategory, "back from main");
 }
