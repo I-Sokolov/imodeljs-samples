d9d40a7da8d307381a7fffc215dbd24ad3637f89
Igor.Sokolov <I.Sokolov@ya.ru>
2020-05-24 14:36:22 +0300

    parse XML full WIP

--- StartingStudy/Console_1/src/Classifications/Repository.ts
+++ StartingStudy/Console_1/src/Classifications/Repository.ts
@@ -10,9 +10,11 @@ import { Logger } from "@bentley/bentleyjs-core";
 
 import { TheApp } from "./TheApp"
 
-/** Access to classification repositories
+
+/** Classification repository
 * https://www.graphisoft.com/downloads/archicad/BIM_Data.html
-* https://developer.mozilla.org/en-US/docs/Web/Guide/Parsing_and_serializing_XML
+* https://usefulangle.com/post/106/nodejs-read-xml
+* https://github.com/Leonidas-from-XIV/node-xml2js 
 * @private
 */
 export class Repository {
@@ -22,22 +24,24 @@ export class Repository {
   /** */
   private filePath: string;
   private parsed: boolean;
-  private xmldoc: Document|null;
+
+  /** */
+  private systems: Array<System>;
 
   /**
    * constructor.
    * @param imode The impdel to work with.
    */
-  public constructor(theApp: TheApp, filePath:string) {
+  public constructor(theApp: TheApp, filePath: string) {
     this.theApp = theApp;
     this.filePath = filePath;
     this.parsed = false;
-    this.xmldoc = null;
+    this.systems = new Array<System>();
 
     this.Parse();
   }
 
-/** */
+  /** */
   private Parse() {
     if (this.parsed)
       return;
@@ -46,21 +50,94 @@ export class Repository {
     try {
       this.theApp.Trace(`Parsing ${this.filePath}`);
       const str: string = fs.readFileSync(this.filePath, "utf8");
-      const parser = new xml.Parser()
-      parser.parseString(str, this.OnParse);
-      //this.xmldoc = parser.parseFromString(str, "application/xml");
+      const parser = new xml.Parser();
+      parser.parseString(str, (err:any, xml:any)=>this.OnXMLParsed(err,xml));
     }
     catch (err) {
       Logger.logException(this.theApp.loggerCategory, err);
     }
   }
 
-  private OnParse(err: any, data: any) {
+  /** */
+  private OnXMLParsed(err: any, xml: any) {
     if (err)   {
-      console.dir(err);
+      throw err;
     }
     else {
-      console.dir(data);
+      const buildingInformation = xml.BuildingInformation;
+      for (const classification of buildingInformation.Classification) {
+        for (const xmlSystem of classification.System) {
+          const sys = new System(this.theApp, xmlSystem);
+          this.systems.push(sys);
+        }
+      }
     }
+  }
+}
+
+/** Classification system in repository */
+export class System {
+  /** */
+  private theApp: TheApp;
+
+  /** */
+  private name: string;
+  private editionVersion: string;
+  private editionDate: Date;
+  private description: string;
+  private source: string;
+
+  /** */
+  private items: Array<Item>;
+
+  /** */
+  constructor(theApp: TheApp, xmlNode: any) {
+    this.theApp = theApp;
+
+    this.name = xmlNode.Name;
+    this.editionVersion = xmlNode.editionVersion;
+    this.editionDate = new Date();
+    this.editionDate.setFullYear(xmlNode.editionDate.Year);
+    this.editionDate.setMonth(xmlNode.editionDate.setMonth);
+    this.editionDate.setDate(xmlNode.editionDate.Day);
+    this.description = xmlNode.description;
+    this.source = xmlNode.source;
+
+    this.items = new Array<Item>();
+    for (const xmlItem of xmlNode.items) {
+      const item = new Item(theApp, xmlItem);
+      this.items.push(item);
+    }
+  }
+
+}
+
+/** Access to classification system in repository */
+export class Item {
+  /** */
+  private theApp: TheApp;
+
+  /** */
+  private id: string;
+  private name: string;
+  private description: string;
+
+  /** */
+  private children: Array<Item>;
+
+  /** */
+  constructor(theApp: TheApp, xmlNode: any) {
+    this.theApp = theApp;
+
+    this.id = xmlNode.id;
+    this.name = xmlNode.name;
+    this.description = xmlNode.description;
+
+    this.children = new Array<Item>();
+    for (const xmlItem of xmlNode.items) {
+      const item = new Item(theApp, xmlItem);
+      this.children.push(item);
+    }
+
   }
 }
