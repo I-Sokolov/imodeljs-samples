ecce400240b1af4191dd3cf86f3fad9017d158ce
Igor.Sokolov <I.Sokolov@ya.ru>
2020-06-07 17:14:56 +0300

    grab from Graphisoft

--- StartingStudy/Console_1/src/Classifications/GetGraphisoftRepositories.ts
+++ StartingStudy/Console_1/src/Classifications/GetGraphisoftRepositories.ts
@@ -1,18 +1,79 @@
 import * as rq from "request"
 import * as fs from "fs"
 import * as rqp from "request-promise-native"
+import * as ch from "cheerio"
+import * as path from "path"
 
 export class Download {
 
-  private RequestCallback(error: any, response: rq.Response, body: any) {
-    console.error('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.    
-  }
+  private urls : Array<string> = new Array<string> ();
 
   public async Run() {
-    const request = await rqp.get("https://www.graphisoft.com/downloads/archicad/BIM_Data.html", this.RequestCallback);
-    request.pipe(fs.createWriteStream('E:\\DevArea\\GitHub\\imodeljs-samples\\test.txt'));
+    try {
+      const url = "https://www.graphisoft.com/downloads/archicad/BIM_Data.html";
+    
+      console.log("Getting from " + url);
+
+      await rqp.get(url, (error: any, response: rq.Response, body: any) => this.RequestCallback (error, response, body));
+
+      for (const url of this.urls) {
+        await this.GetFile (url);
+      }
+
+      console.log("Finish processing " + url);
+    }
+    catch (err) {
+      console.error(err);
+    }
+
+  }
+
+  private RequestCallback(error: any, response: rq.Response, body: any) {
+    if (error) {
+      console.error('error:', error); // Print the error if one occurred
+    }
+    else {
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      this.ParseGraphisoftResponese(body);
+    }
+  }
+
+  private ParseGraphisoftResponese(response: string) {
+    //console.log(response); 
+    const $ = ch.load(response);
+    const links = $("a");
+    //console.log(links);
+    links.each((i: number, link: CheerioElement) => {
+      const url: string | undefined= $(link).attr("href");
+      if (url && url.endsWith(".xml")) {
+        console.log(`Found ${i}: ${url}`);
+        this.urls.push(url);
+      }
+    });
+  }
+
+  private async GetFile(url: string) {
+    console.log(`Requesting ${url}`);
+
+    await rqp.get(url, (error: any, response: rq.Response, body: any) => {
+      if (error) {
+        console.error(error);
+      }
+      else {
+        console.log(response); 
+
+        const repoPath = "./src/Classifications/SavedRepositories";
+        const basename = path.basename(url);
+        
+        const filePath = path.join(repoPath, basename);
+        
+        fs.writeFileSync(filePath, body);
+        
+        console.log(`Saved as ${filePath}`);
+      }        
+    }
+    );
+
   }
 
 }
